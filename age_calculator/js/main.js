e6db10009d43aab5e4b9e3a8ec99b5941f3277b5
kayAmir <79874770+kayAmir@users.noreply.github.com>
2021-03-22 12:40:05 +0200

    Source Files
    these are the files of the main script and they are
    1/ index.html (html file)
    2/ style.css (css file)
    3/ main.js (the javascript file).
    22/3/2021 
    khalid amir

---
+++ age_calculator/js/main.js
@@ -0,0 +1,20 @@
+var myAge = prompt("What is your age?");
+
+document.write("Your age is: " + myAge + " years" + "<br>" );
+
+var ageInDays = myAge * 365 ;
+
+document.write("Your age in days is: " + ageInDays  + " days" + "<br>");
+
+var ageInHours = ageInDays * 24;
+
+document.write("Your Age in Hours is: " + ageInHours  + " hours" + "<br>");
+
+var ageInMinutes = ageInHours * 60;
+
+document.write("Your Age In Minutes Is: " + ageInMinutes  + " minutes" + "<br>");
+
+var ageInSeconds = ageInMinutes * 60;
+
+document.write("Your Age In Seconds Is: " + ageInSeconds + " seconds" + "<br>");
+
